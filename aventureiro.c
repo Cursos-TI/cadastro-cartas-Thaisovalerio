9f9717f6ee75ecbcebb70d517dba7c0fc7519430
Thaisovalerio <thaisovalerio@gmail.com>
2026-06-29 19:13:56 -0300

    Fiz o desafio nível aventureiro
    This program collects and processes data about two cities, including state, code, population, area, GDP, and number of tourist points, and then calculates density and GDP per capita.

---
+++ aventureiro.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+//Declaração de variáveis    
+char estado1, estado2;
+char codigo1[4], codigo2[4];
+char cidade1[50], cidade2[50];
+int populacao1, populacao2;
+float area1, area2;
+float PIB1, PIB2;
+int npturistico1, npturistico2;
+
+float densidade1, densidade2;
+float pibpercapita1, pibpercapita2;
+
+// Entrada de dados
+printf("Carta 1: \n");
+printf("Digite o estado: ");
+scanf("%c", &estado1);
+
+printf("Digite o codigo: ");
+scanf("%s", &codigo1);
+
+getchar(); //Consome o Enter deixado pelo scanf
+
+printf("Digite a cidade: ");
+fgets(cidade1,50,stdin);
+cidade1[strcspn(cidade1, "\n")]=0; //Remove o enter guardado pelo fgets
+
+printf("Digite a populacao: ");
+scanf("%d", &populacao1);
+
+printf("Digite a area: ");
+scanf("%f", &area1);
+
+printf("Digite o PIB: ");
+scanf("%f", &PIB1);
+
+printf("Digite o numero de pontos turisticos: ");
+scanf("%d", &npturistico1);
+
+printf("Carta 2: \n");
+
+getchar(); // Consome o Enter
+
+printf("Digite o Estado: ");
+scanf("%c", &estado2);
+
+printf("Digite o codigo: ");
+scanf("%s", &codigo2);
+
+getchar(); // Consome o Enter deixado pelo scanf
+
+printf("Digite a cidade: ");
+fgets(cidade2,50,stdin);
+cidade2[strcspn(cidade2, "\n")]=0; //Remover o Enter que ficou salvo pelo fgets
+
+printf("Digite a populacao: ");
+scanf("%d", &populacao2);
+
+printf("Digite a area: ");
+scanf("%f", &area2);
+
+printf("Digite o PIB: ");
+scanf("%f", &PIB2);
+
+printf("Digite o numero de pontos turisticos: ");
+scanf("%d", &npturistico2);
+
+//Processamento dos dados
+densidade1 = populacao1 / area1;
+densidade2 = populacao2 / area2;
+pibpercapita1 = (PIB1*1000000000) / populacao1;
+pibpercapita2 = (PIB2*1000000000) / populacao2;
+
+//Saída de dados
+printf("Cidade 1: )\n");
+printf("Estado: %c\n", estado1);
+printf("Codigo: %s\n", codigo1);
+printf("Nome da cidade: %s\n", cidade1);
+printf("Populacao: %d\n", populacao1);
+printf("Area: %.2f Km\n", area1);
+printf("PIB: %.2f bilhoes de reais\n", PIB1);
+printf("Numero de Pontos Turisticos: %d\n", npturistico1);
+printf("Densidade populacional: %.2f hab/km\n",densidade1);
+printf("PIB Per Capita: %.2f reais\n",pibpercapita1);
+
+printf("Carta 2: \n");
+printf("Estado: %c\n", estado2);
+printf("Codigo: %s\n", codigo2);
+printf("Nome da cidade: %s\n", cidade2);
+printf("Populacao: %d\n", populacao2);
+printf("Area: %.2f km\n", area2);
+printf("PIB: %.2f bilhoes de reais\n", PIB2);
+printf("Numero de Pontos Turisticos: %d\n", npturistico2);
+printf("Densidade populacional: %.2f hab/km\n",densidade2);
+printf("PIB Per Capita: %.2f reais\n",pibpercapita2);
+
+return 0;
+
+
+
+}
